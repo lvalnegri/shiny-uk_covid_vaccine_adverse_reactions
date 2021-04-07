b0a168e49bb5e57ff018d8d3ce98dd501835edea
Luca Valnegri <l.valnegri@datamaps.co.uk>
2021-04-07 19:03:17 +0100

    - added total for filtered total table
     - dropped ranks fields

--- app.R
+++ app.R
@@ -2,11 +2,19 @@ dmpkg.funs::load_pkgs(c('data.table', 'DT', 'htmltools', 'shiny'))
 
 dpath <- file.path(datauk_path, 'covid', 'vaccine')
 dts <- fst::read_fst(file.path(dpath, 'vaccine_adverse_reactions'), as.data.table = TRUE)
+dts[, rnk := NULL]
 up_date <- format(as.Date(readLines(file.path(dpath, 'vaccine_adverse_reactions.date'))), '%d %B %Y')
+
+js_footer <- function(x){
+    paste0(
+        'tot_', x, ' = api.column(', x, ', {search:"applied", page:"all"}).data().reduce(function(a, b){return a + b;})
+        $(api.column(', x, ').footer()).html(tot_', x, '.toLocaleString());'
+    )
+}
 
 ui <- fluidPage(
 
-    titlePanel('UK Covid Vaccines Adverse Reactions'), br(),
+    titlePanel('UK Covid Vaccines Yellow Card Reporting (potential adverse reactions)'), br(),
 
     shinyWidgets::radioGroupButtons('rdb_grp', 'AGGREGATION:', 
         choices = c('None', 'Group', 'Class', 'Brand'),
@@ -23,8 +31,8 @@ ui <- fluidPage(
 
 server <- function(input, output, session){
 
-    output$out_tbl <- renderDT({
-
+    dtbl <- reactive({
+        
         switch(input$rdb_grp,
 
             'Group' = { 
@@ -41,8 +49,10 @@ server <- function(input, output, session){
                                 th(colspan = 2, span('Pfizer', style = "color:cyan; font-family:'times'; font-size:20pt; display:table; margin:0 auto;") )
                             ),
                             tr( th('Total'), th('Fatal'), th('Total'), th('Fatal') )
-                        )
+                        ),
+                        tableFooter(c('', 'TOTAL TABLE: ', 0, 0, 0, 0))
                 ))
+                foot_dt <- paste(js_footer(2), js_footer(3), js_footer(4), js_footer(5))
             },
 
             'Class' = { 
@@ -61,25 +71,25 @@ server <- function(input, output, session){
                                 th('Total', style = "text-align: center"), th('Fatal', style = "text-align: center"), 
                                 th('Total', style = "text-align: center"), th('Fatal', style = "text-align: center")
                             )
-                        )
+                        ),
+                        tableFooter(c('TOTAL TABLE: ', 0, 0, 0, 0))
                 ))
+                foot_dt <- paste(js_footer(1), js_footer(2), js_footer(3), js_footer(4))
             },
 
             'Brand' = { 
                 y <- dts[, .(Total = sum(Total), Fatal = sum(Fatal)), Brand] 
                 sketch <- withTags(table(
                     class = 'display',
-                    thead( tr( th(colspan = 1, 'Brand'), th(colspan = 1, 'Total'), th(colspan = 1, 'Fatal') ))
+                    thead( tr( th(colspan = 1, 'Brand'), th(colspan = 1, 'Total'), th(colspan = 1, 'Fatal') )),
+                    tableFooter(c('TOTAL TABLE: ', 0, 0))
                 ))
+                foot_dt <- paste(js_footer(1), js_footer(2))
             },
 
             { 
-                y <- dcast(dts, Class+Group+Reaction~Brand, value.var = c('Total', 'Fatal', 'rnk'), fill = 0)
-                setcolorder(y, c(
-                    'Class', 'Group', 'Reaction', 
-                    'Total_AstraZeneca', 'Fatal_AstraZeneca', 'rnk_AstraZeneca',
-                    'Total_Pfizer', 'Fatal_Pfizer', 'rnk_Pfizer'
-                ))
+                y <- dcast(dts, Class+Group+Reaction~Brand, value.var = c('Total', 'Fatal'), fill = 0)
+                setcolorder(y, c('Class', 'Group', 'Reaction', 'Total_AstraZeneca', 'Fatal_AstraZeneca', 'Total_Pfizer', 'Fatal_Pfizer'))
                 sketch <- withTags(table(
                         class = 'display',
                         thead(
@@ -87,12 +97,14 @@ server <- function(input, output, session){
                                 th(rowspan = 2, 'Class'),
                                 th(rowspan = 2, 'Group'),
                                 th(rowspan = 2, 'Reaction'),
-                                th(colspan = 3, span('AstraZeneca', style = "color:gold; font-family:'times'; font-size:20pt; display:table; margin:0 auto;") ),
-                                th(colspan = 3, span('Pfizer', style = "color:cyan; font-family:'times'; font-size:20pt; display:table; margin:0 auto;") )
+                                th(colspan = 2, span('AstraZeneca', style = "color:gold; font-family:'times'; font-size:20pt; display:table; margin:0 auto;") ),
+                                th(colspan = 2, span('Pfizer', style = "color:cyan; font-family:'times'; font-size:20pt; display:table; margin:0 auto;") )
                             ),
-                            tr( th('Total'), th('Fatal'), th('Rank'), th('Total'), th('Fatal'), th('Rank') )
-                        )
+                            tr( th('Total'), th('Fatal'), th('Total'), th('Fatal') )
+                        ),
+                        tableFooter(c('', '', 'TOTAL TABLE: ', 0, 0, 0, 0))
                 ))
+                foot_dt <- paste(js_footer(3), js_footer(4), js_footer(5), js_footer(6))
             }
         
         )
@@ -115,7 +127,7 @@ server <- function(input, output, session){
                 ))))
             ),
             options = list(
-                scrollX = TRUE,
+#                scrollX = TRUE,
                 scrollY = 400,
                 scroller = TRUE,
                 ordering = TRUE,
@@ -124,9 +136,15 @@ server <- function(input, output, session){
                 buttons = c('copy', 'csv', 'print'),
                 initComplete = JS(
                     "function(settings, json) {",
-                    "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff'});",
+                        "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff', 'text-align': 'center', 'font-size': '120%'});",
+                        "$(this.api().table().footer()).css({'background-color': '#000', 'color': '#fff', 'text-align': 'center', 'font-size': '120%'});",
                     "}"
                 ),
+                footerCallback = JS(paste0(
+                    "function( tfoot, data, start, end, display ) {",
+                        "var api = this.api(), data;", foot_dt,
+                    "}"
+                )),
                 dom = 'Biftp'
             )
         )
@@ -138,8 +156,11 @@ server <- function(input, output, session){
         }
 
         dt
- 
+        
+        
     })
+
+    output$out_tbl <- renderDT( dtbl(), server = FALSE )
 
 }
 
